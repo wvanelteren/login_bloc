1c5365bcff617c612a299f09fd4838c63925ca18
Wouter van Elteren <33261231+wvanelteren@users.noreply.github.com>
2020-03-18 20:55:29 +0100

    Update validators.dart

--- lib/src/blocs/validators.dart
+++ lib/src/blocs/validators.dart
@@ -10,7 +10,7 @@ class Validators {
 
     final validatePassword = StreamTransformer<String, String>.fromHandlers(
     handleData: (password, sink) {
-      return password.contains('@') ? sink.add(password) : sink.addError('Password must be at least 4 characters');
+      return password.length > 3 ? sink.add(password) : sink.addError('Password must be at least 4 characters');
     }
   );
 
